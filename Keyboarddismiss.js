1d5aea1543f4bab1dec5700e01b9cf5bcd4b8584
Molindu Yasoda <54410056+molindu@users.noreply.github.com>
2023-10-18 22:14:22 +0530

    Update Keyboarddismiss.js

--- Keyboarddismiss.js
+++ Keyboarddismiss.js
@@ -1,2 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.textInputRef = React.createRef();
+  }
+
+  handleTouchOutside = () => {
+    // Dismiss the keyboard when the user touches outside the input field
+    Keyboard.dismiss();
+  };
+
+  render() {
+    return (
+      <TouchableWithoutFeedback onPress={this.handleTouchOutside}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <TextInput
+            ref={this.textInputRef}
+            style={{ borderColor: 'gray', borderWidth: 1, padding: 10, width: 200 }}
+            placeholder="Type something..."
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+export default MyComponent;
